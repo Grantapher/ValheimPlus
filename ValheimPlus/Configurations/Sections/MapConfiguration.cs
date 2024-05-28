aa370f6096446b335088c3dcc62fe84a7b3bb6a7
VplusR <140290624+VplusR@users.noreply.github.com>
2024-05-28 16:54:36 -0400

    Update MapConfiguration.cs

--- ValheimPlus/Configurations/Sections/MapConfiguration.cs
+++ ValheimPlus/Configurations/Sections/MapConfiguration.cs
@@ -7,7 +7,7 @@
         public bool preventPlayerFromTurningOffPublicPosition { get; internal set; } = false;
         
         //relics from the old vplus map pin UI
-        //public bool useImprovedPinEditorUI { get; internal set; } = false;
+        public bool useImprovedPinEditorUI { get; internal set; } = false;
         public bool shareablePins { get; internal set; } = false;  // i think this needs to be on for shareAllPins to work
         public bool shareAllPins { get; internal set; } = false;
         public bool displayCartsAndBoats { get; internal set; } = false;
