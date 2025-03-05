8fc2f0530449d2df681c6d3a0cbda26d57e7f0a4
beemerwt <beemerwt@gmail.com>
2025-03-04 22:24:07 -0600

    Added ship modifications

---
+++ ValheimPlus/Configurations/Sections/ShipConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ValheimPlus.Configurations.Sections
+{
+    // VikingShip is the prefab, behaviour is Ship
+    public class ShipConfiguration : ServerSyncConfig<ShipConfiguration>
+    {
+
+        // m_sailForceFactor
+        public float sailForce { get; internal set; } = 0f;
+
+        // m_sailForceOffset
+        public float sailForceOffset { get; internal set; } = 0f;
+
+        // m_stearForce
+        public float steerForce { get; internal set; } = 0f;
+
+        // m_waterImpactDamage, m_minWaterImpactInterval, m_minWaterImpactForce
+        public float waterImpactDamage { get; internal set; } = 0f;
+
+        // m_waterLevelOffset
+        public float waterLevel { get; internal set; } = 0f;
+
+        // m_force, m_forceDistance, m_damping, m_dampingSideway, m_dampingForward, m_angularDamping
+
+
+        // m_backwardForce
+        public float backwardForce { get; internal set; } = 0f;
+
+        // m_rudderSpeed
+        public float rudderSpeed { get; internal set; } = 0f;
+    }
+}
