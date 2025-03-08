9957fe05d21044987b3e2e8c520a136a66e7de4d
beemerwt <beemerwt@gmail.com>
2025-03-08 07:27:56 -0600

    revert change to itemStackMultiplier

--- ValheimPlus/Configurations/Sections/ItemsConfiguration.cs
+++ ValheimPlus/Configurations/Sections/ItemsConfiguration.cs
@@ -4,7 +4,7 @@
     {
         public bool noTeleportPrevention { get; internal set; } = false;
         public float baseItemWeightReduction { get; internal set; } = 0;
-        public float itemStackMultiplier { get; internal set; } = 0;
+        public float itemStackMultiplier { get; internal set; } = 1;
         public float droppedItemOnGroundDurationInSeconds { get; internal set; } = 3600;
         public bool itemsFloatInWater { get; internal set; } = false;
     }
