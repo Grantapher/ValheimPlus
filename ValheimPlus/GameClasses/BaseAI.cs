7cabc095b2cd9fd44567a81287c7cbc5a680f79e
beemerwt <beemerwt@gmail.com>
2025-03-11 11:06:18 -0500

    Added BaseAIHelpers

---
+++ ValheimPlus/GameClasses/BaseAI.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using System;
+using System.Reflection.Emit;
+
+namespace ValheimPlus.GameClasses
+{
+	public static class BaseAIHelpers
+	{
+		private static bool IsAlerted(BaseAI baseAi) => false;
+
+		public static CodeMatcher IgnoreAlertedTranspiler(CodeMatcher matcher, Func<Tameable, bool> pred = null)
+		{
+			var ignoreAlertedMethod = AccessTools.Method(typeof(BaseAIHelpers), nameof(IsAlerted));
+			var baseAiIsAlerted = AccessTools.Method(typeof(BaseAI), nameof(BaseAI.IsAlerted));
+			return matcher
+				.MatchStartForward(new CodeMatch(inst => inst.Calls(baseAiIsAlerted)))
+				.ThrowIfNotMatch("No match for IsAlerted method call.")
+				.Set(OpCodes.Call, pred?.Method ?? ignoreAlertedMethod)
+				.Start();
+		}
+	}
+
+	// if ((!this.IsAlerted()
+	// || (UnityEngine.Object) this.m_targetStatic == (UnityEngine.Object) null && (UnityEngine.Object) this.m_targetCreature == (UnityEngine.Object) null)
+	// && this.UpdateConsumeItem(character, dt))
+}
