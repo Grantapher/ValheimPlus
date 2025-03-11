95e091d7623650626fc9bb99c8d6062149ced799
beemerwt <beemerwt@gmail.com>
2025-03-11 11:05:50 -0500

    extra linter fixes

--- ValheimPlus/GameClasses/Tameable.cs
+++ ValheimPlus/GameClasses/Tameable.cs
@@ -11,12 +11,12 @@ using ValheimPlus.Utility;
 
 namespace ValheimPlus.GameClasses
 {
-    public enum TameableMortalityTypes
-    {
-        Normal,
-        Essential,
-        Immortal
-    }
+	public enum TameableMortalityTypes
+	{
+		Normal,
+		Essential,
+		Immortal
+	}
 
 	public static class TameableHelpers
 	{
@@ -34,41 +34,41 @@ namespace ValheimPlus.GameClasses
 		}
 	}
 
-    /// <summary>
-    /// Adds a text indicator so player's know when an animal they've tamed has been stunned.
-    /// </summary>
-    [HarmonyPatch(typeof(Tameable), nameof(Tameable.GetHoverText))]
-    public static class Tameable_GetHoverText_Patch
-    {
-        public static void Postfix(Tameable __instance, ref string __result)
-        {
+	/// <summary>
+	/// Adds a text indicator so player's know when an animal they've tamed has been stunned.
+	/// </summary>
+	[HarmonyPatch(typeof(Tameable), nameof(Tameable.GetHoverText))]
+	public static class Tameable_GetHoverText_Patch
+	{
+		public static void Postfix(Tameable __instance, ref string __result)
+		{
 			if (Configuration.Current.Tameable.IsEnabled && Configuration.Current.Tameable.stunInformation)
-            {
-                // If tamed creature is recovering from a stun, then add Stunned to hover text.
-                if (__instance.m_character.m_nview.GetZDO().GetBool("isRecoveringFromStun"))
-                    __result = __result.Insert(__result.IndexOf(" )"), ", Stunned");
-            }
+			{
+				// If tamed creature is recovering from a stun, then add Stunned to hover text.
+				if (__instance.m_character.m_nview.GetZDO().GetBool("isRecoveringFromStun"))
+					__result = __result.Insert(__result.IndexOf(" )"), ", Stunned");
+			}
 
 			var procreation = __instance.GetComponent<Procreation>();
 			if (procreation != null)
 				ProcreationHelper.AddLoveInformation(__instance, procreation, ref __result);
-        }
-    }
+		}
+	}
 
-    [HarmonyPatch(typeof(Tameable), nameof(Tameable.Awake))]
+	[HarmonyPatch(typeof(Tameable), nameof(Tameable.Awake))]
 	public static class Tameable_Awake_Patch
-    {
+	{
 		public static void Postfix(Tameable __instance)
 		{
-            if (!Configuration.Current.Tameable.IsEnabled)
-                return;
+			if (!Configuration.Current.Tameable.IsEnabled)
+				return;
 
-            __instance.m_tamingTime = Configuration.Current.Tameable.tameTime;
-            __instance.m_tamingSpeedMultiplierRange = Configuration.Current.Tameable.tameBoostRange;
-            __instance.m_tamingBoostMultiplier = Configuration.Current.Tameable.tameBoostMultiplier;
+			__instance.m_tamingTime = Configuration.Current.Tameable.tameTime;
+			__instance.m_tamingSpeedMultiplierRange = Configuration.Current.Tameable.tameBoostRange;
+			__instance.m_tamingBoostMultiplier = Configuration.Current.Tameable.tameBoostMultiplier;
 
 			if (__instance.m_nview.GetZDO().GetFloat(ZDOVars.s_tameTimeLeft, out float timeLeft))
-                if (timeLeft > __instance.m_tamingTime)
+				if (timeLeft > __instance.m_tamingTime)
 					__instance.m_nview.GetZDO().Set(ZDOVars.s_tameTimeLeft, __instance.m_tamingTime);
 		}
 	}
