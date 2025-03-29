c35c50c3a1d2ec8c66f21ce69277e1e9b6c09ff9
beemerwt <beemerwt@gmail.com>
2025-03-29 10:39:05 -0500

    removed predicate argument

--- ValheimPlus/GameClasses/BaseAI.cs
+++ ValheimPlus/GameClasses/BaseAI.cs
@@ -4,19 +4,19 @@ using System.Reflection.Emit;
 
 namespace ValheimPlus.GameClasses
 {
-	public static class BaseAIHelpers
-	{
-		private static bool IsAlerted(BaseAI baseAi) => false;
+    public static class BaseAIHelpers
+    {
+        private static bool IgnoreAlerted(BaseAI baseAi) => false;
 
-		public static CodeMatcher IgnoreAlertedTranspiler(CodeMatcher matcher, Func<Tameable, bool> pred = null)
-		{
-			var ignoreAlertedMethod = AccessTools.Method(typeof(BaseAIHelpers), nameof(IsAlerted));
-			var baseAiIsAlerted = AccessTools.Method(typeof(BaseAI), nameof(BaseAI.IsAlerted));
-			return matcher
-				.MatchStartForward(new CodeMatch(inst => inst.Calls(baseAiIsAlerted)))
-				.ThrowIfNotMatch("No match for IsAlerted method call.")
-				.Set(OpCodes.Call, pred?.Method ?? ignoreAlertedMethod)
-				.Start();
-		}
-	}
+        public static CodeMatcher IgnoreAlertedTranspiler(CodeMatcher matcher)
+        {
+            var ignoreAlertedMethod = AccessTools.Method(typeof(BaseAIHelpers), nameof(IgnoreAlerted));
+            var baseAiIsAlerted = AccessTools.Method(typeof(BaseAI), nameof(BaseAI.IsAlerted));
+            return matcher
+                .MatchStartForward(new CodeMatch(inst => inst.Calls(baseAiIsAlerted)))
+                .ThrowIfNotMatch("No match for IsAlerted method call.")
+                .Set(OpCodes.Call, ignoreAlertedMethod)
+                .Start();
+        }
+    }
 }
