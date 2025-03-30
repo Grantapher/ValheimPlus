139235e714608b72fc6523cb8943cb54073cdf73
beemerwt <beemerwt@gmail.com>
2025-03-30 07:01:20 -0500

    Added helper function for transpiler

--- ValheimPlus/GameClasses/BaseAI.cs
+++ ValheimPlus/GameClasses/BaseAI.cs
@@ -1,19 +1,38 @@
 ﻿using HarmonyLib;
+using System;
+using System.Reflection.Emit;
 
 namespace ValheimPlus.GameClasses
 {
-    [HarmonyPatch(typeof(BaseAI), nameof(BaseAI.IsAlerted))]
-    public static class BaseAI_IsAlerted_Patch
+    public static class BaseAIHelpers
     {
-        public static void Postfix(BaseAI __instance, ref bool __result)
+        /// <summary>
+        /// Appends `BaseAI.m_character.m_name` to the first `BaseAI.IsAlerted` call
+        /// Calls the provided predicate with the character name
+        /// Then inverts the result and ANDs it with the original result
+        /// This allows the original `BaseAI.IsAlerted` method to be called for compatibility
+        /// And exceptions to be made for any `BaseAI.m_character.m_name` that matches the predicate
+        /// </summary>
+        /// <param name="matcher">The current transpiler matcher</param>
+        /// <param name="pred">BaseAI.m_character.m_name filter</param>
+        /// <returns>The current transpiler matcher</returns>
+        public static CodeMatcher IsAlertedTranspiler(CodeMatcher matcher, Func<string, bool> pred)
         {
-            if (!__instance.m_character.m_tameable)
-                return;
-
-            var isTamed = __instance.m_character.IsTamed();
-            __result = __result && !(isTamed ?
-                ProcreationHelpers.ShouldIgnoreAlerted(__instance)
-                : TameableHelpers.ShouldIgnoreAlerted(__instance));
+            var isAlertedMethod = AccessTools.Method(typeof(BaseAI), nameof(BaseAI.IsAlerted));
+            var baseAICharacterField = AccessTools.Field(typeof(BaseAI), nameof(BaseAI.m_character));
+            var characterNameField = AccessTools.Field(typeof(Character), nameof(Character.m_name));
+            matcher.MatchStartForward(new CodeMatch(inst => inst.Calls(isAlertedMethod)))
+                .ThrowIfNotMatch("Could not find BaseAI.IsAlerted call")
+                .Advance(1)
+                .InsertAndAdvance(
+                    new(OpCodes.Ldarg_0),
+                    new(OpCodes.Ldfld, baseAICharacterField),
+                    new(OpCodes.Ldfld, characterNameField),
+                    new(OpCodes.Call, pred.Method),
+                    new(OpCodes.Not),
+                    new(OpCodes.And)
+                );
+            return matcher;
         }
     }
 }
