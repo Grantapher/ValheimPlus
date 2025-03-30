dd6814250d8cd592d0943f96d2ab2e2ae92a507f
beemerwt <beemerwt@gmail.com>
2025-03-29 21:10:59 -0500

    Added animal type to tameable config

--- ValheimPlus/Configurations/Sections/TameableConfiguration.cs
+++ ValheimPlus/Configurations/Sections/TameableConfiguration.cs
@@ -1,7 +1,10 @@
-﻿namespace ValheimPlus.Configurations.Sections
+﻿using ValheimPlus.GameClasses;
+
+namespace ValheimPlus.Configurations.Sections
 {
-	public class TameableConfiguration : ServerSyncConfig<TameableConfiguration>
+    public class TameableConfiguration : ServerSyncConfig<TameableConfiguration>
     {
+        public AnimalType animalTypes { get; internal set; } = AnimalType.All;
         public int mortality { get; internal set; } = 0;
         public bool ownerDamageOverride { get; internal set; } = false;
         public float stunRecoveryTime { get; internal set; } = 10f;
@@ -9,7 +12,7 @@
         public float tameTime { get; internal set; } = 1800f;
         public float tameBoostRange { get; internal set; } = 60f;
         public float tameBoostMultiplier { get; internal set; } = 2f;
-		public bool ignoreHunger { get; internal set; } = false;
-		public bool ignoreAlerted { get; internal set; } = false;
-	}
+        public bool ignoreHunger { get; internal set; } = false;
+        public bool ignoreAlerted { get; internal set; } = false;
+    }
 }
